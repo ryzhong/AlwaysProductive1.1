9222c1c662613c75b075bb31083f33c92210ff31
Ryan Zhong <ryanzhong@Ryans-MacBook-Pro.local>
2020-02-04 16:17:22 -0800

    Added greeting and link to logout user

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,18 +1,36 @@
 import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'
+import * as firebase from 'firebase'
 
 export default class HomeScreen extends React.Component {
+    state = {
+        email: "",
+        displayName: ""
+    }
+
+    componentDidMount() {
+        const {email, displayName} = firebase.auth().currentUser;
+
+        this.setState( {email, displayName});
+    }
+
+    signOutUser = () => {
+        firebase.auth().signOut();
+    }
+
     render() {
         return (
             <View style={style.container}>
-                <Text>Home Screen</Text>
+                <Text>Hi {this.displayName}!</Text>
+
+                <TouchableOpacity style={{marginTop: 32}} onPress={this.signOutUser}>
+                    <Text>Logout</Text>
+                </TouchableOpacity>
             </View>
 
         )
     }
 }
-
-//testing merge
 
 const style = StyleSheet.create({
     container: {
