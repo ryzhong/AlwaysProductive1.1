805d24c78272ed90509128ff99628cf6a23dd156
Ryan Zhong <ryanzhong@Ryans-MacBook-Pro.local>
2020-02-11 17:02:47 -0800

    Added header with back and post, added avatar with text input for new tasks

--- screens/AddScreen.js
+++ screens/AddScreen.js
@@ -1,20 +1,56 @@
 import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, SafeAreaView, TouchableOpacity, Image, TextInput} from 'react-native'
+import {Ionicons} from '@expo/vector-icons'
 
 export default class AddScreen extends React.Component {
     render() {
         return (
-            <View style={styles.container}>
-                <Text>Add Screen</Text>
-            </View>
+            <SafeAreaView style={styles.container}>
+                <View style={styles.header}>
+                    <TouchableOpacity>
+                        <Ionicons name="md-arrow-back" size={24} color="#D8D9DB"></Ionicons>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Text style={{ fontWeight: "700" }}>Post</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={styles.inputContainer}>
+                    <Image source={require("../assets/ryanShoo.jpg")} style={styles.avatar}></Image>
+                    <TextInput 
+                        autoFocus={true}
+                        multiline={true}
+                        numberOfLines={4}
+                        style={{ flex : 1 }}
+                        placeholder="Add a task for today"
+                    ></TextInput>
+                </View>
+            </SafeAreaView>
         )
     }
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center"
+        flex: 1
+    },
+    header: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        paddingHorizontal: 32,
+        paddingVertical: 12,
+        borderBottomWidth: 1,
+        top: 20,
+        borderBottomColor: "#D8D9DB"
+    },
+    inputContainer: {
+        margin: 32,
+        flexDirection: "row"
+    },
+    avatar: {
+        width: 48,
+        height: 48,
+        borderRadius: 24,
+        marginRight: 16
     }
 })
