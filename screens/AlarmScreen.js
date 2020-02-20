77a9b668804e6d13f02771d823bd192f96bc827b
Ryan Zhong <ryanzhong@Ryans-MacBook-Pro.local>
2020-02-19 17:09:06 -0800

    Added inputs for hr, min, secs. Added UI to line up

--- screens/AlarmScreen.js
+++ screens/AlarmScreen.js
@@ -1,11 +1,35 @@
 import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, TextInput, TouchableOpacity} from 'react-native'
 
 export default class AlarmScreen extends React.Component {
+    state = {
+        time: {
+            hr: 0,
+            min: 0,
+            sec: 0
+        }
+    }
+
     render() {
         return (
             <View style={styles.container}>
                 <Text>Alarm Screen</Text>
+                <View style={styles.setTimer}>
+                    {/* <Text>HERE</Text> */}
+                    <TextInput style={styles.timeInput}></TextInput>
+                    <Text style={styles.colons}>:</Text>
+                    <TextInput style={styles.timeInput}></TextInput>
+                    <Text style={styles.colons}>:</Text>
+                    <TextInput style={styles.timeInput}></TextInput>
+                </View>
+                <View style={styles.timerLabel}>
+                    {/* <Text>HERE</Text> */}
+                    <Text style={{marginLeft: 78}}>H</Text>
+                    
+                    <Text style={{marginLeft: 78}}>M</Text>
+                    
+                    <Text style={{marginLeft: 78}}>S</Text>
+                </View>
             </View>
         )
     }
@@ -13,8 +37,31 @@ export default class AlarmScreen extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
+        top: 250,
         flex: 1,
         alignItems: "center",
-        justifyContent: "center"
+        // justifyContent: "center"
+    },
+    setTimer: {
+        flexDirection: "row",
+        marginTop: 40
+    },
+    timerLabel: {
+        flexDirection: "row",
+        marginTop: 1,
+        right: 40
+    },
+    timeInput: {
+        height: 60,
+        width: 56,
+        borderColor: "#000000",
+        borderWidth: 1,
+        marginLeft: 10,
+        marginRight: 10,
+        fontSize: 45,
+        textAlign: "right"
+    },
+    colons: {
+        fontSize: 40
     }
 })
