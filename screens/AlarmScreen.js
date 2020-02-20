fb3e7b5788b95893b93b5a62b259ea02da185b5c
Ryan Zhong <ryanzhong@Ryans-MacBook-Pro.local>
2020-02-19 18:08:25 -0800

    Added states for time Added function that removes anything that isn't a number from input Added buttons to start, pause, reset

--- screens/AlarmScreen.js
+++ screens/AlarmScreen.js
@@ -1,12 +1,26 @@
 import React from 'react'
-import {View, Text, StyleSheet, TextInput, TouchableOpacity} from 'react-native'
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
 
 export default class AlarmScreen extends React.Component {
     state = {
         time: {
-            hr: 0,
-            min: 0,
-            sec: 0
+            hr: null,
+            min: null,
+            sec: null
+        }
+    }
+
+    onChanged(time, num) {
+        console.log(time, num)
+        num = num.replace(/[^0-9]/g, '')
+        if(time === "hr") {
+            this.setState({hr: num.replace(/[^0-9]/g, '')})
+        }
+        if(time === "min") {
+            this.setState({min: num.replace(/[^0-9]/g, '')})
+        }
+        if(time === "sec") {
+            this.setState({sec: num.replace(/[^0-9]/g, '')})
         }
     }
 
@@ -16,19 +30,59 @@ export default class AlarmScreen extends React.Component {
                 <Text>Alarm Screen</Text>
                 <View style={styles.setTimer}>
                     {/* <Text>HERE</Text> */}
-                    <TextInput style={styles.timeInput}></TextInput>
+                    <TextInput
+                        style={styles.timeInput}
+                        keyboardType='numeric'
+                        maxLength = {2}
+                        onChangeText = {(num)=> this.onChanged("hr", num)}
+                        value={this.state.time.hr}
+                    >
+                        {this.state.time.hr}
+                    </TextInput>
                     <Text style={styles.colons}>:</Text>
-                    <TextInput style={styles.timeInput}></TextInput>
+                    <TextInput 
+                        style={styles.timeInput}
+                        keyboardType='numeric'
+                        maxLength = {2}
+                        onChangeText = {(num)=> this.onChanged("min", num)}
+                        value={this.state.time.min}
+                    >
+                        {this.state.time.hr}
+                    </TextInput>
                     <Text style={styles.colons}>:</Text>
-                    <TextInput style={styles.timeInput}></TextInput>
+                    <TextInput 
+                        style={styles.timeInput}
+                        keyboardType='numeric'
+                        maxLength = {2}
+                        onChangeText = {(num)=> this.onChanged("sec", num)}
+                        value={this.state.time.sec}
+                    >
+                        {this.state.time.hr}
+                    </TextInput>
                 </View>
                 <View style={styles.timerLabel}>
-                    {/* <Text>HERE</Text> */}
-                    <Text style={{marginLeft: 78}}>H</Text>
-                    
-                    <Text style={{marginLeft: 78}}>M</Text>
-                    
-                    <Text style={{marginLeft: 78}}>S</Text>
+                    <Text style={{ marginLeft: 77 }}>H</Text>
+
+                    <Text style={{ marginLeft: 77 }}>M</Text>
+
+                    <Text style={{ marginLeft: 77 }}>S</Text>
+                </View>
+                <View style={styles.buttons}>
+                    <TouchableOpacity style={styles.button}>
+                        <Text>
+                            Start
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button}>
+                        <Text>
+                            Pause
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button}>
+                        <Text>
+                            Reset
+                        </Text>
+                    </TouchableOpacity>
                 </View>
             </View>
         )
@@ -37,7 +91,7 @@ export default class AlarmScreen extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
-        top: 250,
+        top: 200,
         flex: 1,
         alignItems: "center",
         // justifyContent: "center"
@@ -49,19 +103,36 @@ const styles = StyleSheet.create({
     timerLabel: {
         flexDirection: "row",
         marginTop: 1,
-        right: 40
+        right: 39
     },
     timeInput: {
         height: 60,
-        width: 56,
+        width: 60,
         borderColor: "#000000",
         borderWidth: 1,
         marginLeft: 10,
         marginRight: 10,
         fontSize: 45,
-        textAlign: "right"
+        textAlign: "right",
+        padding: 3
     },
     colons: {
         fontSize: 40
+    },
+    buttons: {
+        flexDirection: "row"
+    },
+    button: {
+        marginTop: 50,
+        marginLeft: 30,
+        marginRight: 30,
+        width: 50,
+        height: 30,
+        borderWidth: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#E9446A",
+        borderRadius: 4,
+        marginHorizontal: 30,
     }
 })
